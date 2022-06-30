0c4ee23880e437bd80328a8fd3b5a62935f969a5
RamyaPrabakar <ramyaprabakar01@gmail.com>
2022-06-30 12:12:45 -0700

    getting the timeline to show up in the profile page

--- Instagram/View Controllers/ProfileViewController.m
+++ Instagram/View Controllers/ProfileViewController.m
@@ -6,8 +6,15 @@
 //
 
 #import "ProfileViewController.h"
+#import "Parse/Parse.h"
+#import "PFImageView.h"
+#import "PostCell.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet PFImageView *profilePicture;
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (nonatomic, strong) NSArray *arrayOfPosts;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -15,7 +22,122 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    PFUser *currUser = [PFUser currentUser];
+    self.username.text = currUser[@"username"];
+    // self.profilePicture.image = currUser[@"profilePicture"];
+    
+    self.tableView.dataSource = self;
+    // self.tableView.delegate = self;
+    
+    [self getTimeline];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)chooseProfilePicture:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    NSLog(@"We will use photo library instead");
+    imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (IBAction)takeProfilePicture:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.profilePicture.image = editedImage;
+    
+    PFUser *currUser = [PFUser currentUser];
+    currUser[@"profilePicture"] = [self getPFFileFromImage:self.profilePicture.image];
+    [currUser saveInBackground];
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.6);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    return [PFFileObject fileObjectWithName:@"image.jpeg" data:imageData];
+}
+
+-(void) getTimeline {
+    // construct query
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+    query.limit = 20;
+    [query orderByDescending:@"createdAt"];
+    [query includeKey:@"author"];
+    
+    // fetch data asynchronously
+    [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+        if (posts != nil) {
+            // do something with the array of object returned by the call
+            self.arrayOfPosts = posts;
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    PostCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    Post *currPost = self.arrayOfPosts[indexPath.row];
+    cell.postCaption.text = currPost.caption;
+    
+    cell.postImage.file = currPost[@"image"];
+    [cell.postImage loadInBackground];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    PFUser *user = currPost[@"author"];
+    cell.usernameLabel.text = user.username;
+    
+    NSDate *createdAt = currPost.createdAt;
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    // Configure the input format to parse the date string
+    formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+    
+    // Configure output format
+    formatter.dateStyle = NSDateFormatterShortStyle;
+    formatter.timeStyle = NSDateFormatterNoStyle;
+    
+    // Convert Date to String
+    cell.timestamp.text = [formatter stringFromDate:createdAt];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.arrayOfPosts.count;
 }
 
 /*
